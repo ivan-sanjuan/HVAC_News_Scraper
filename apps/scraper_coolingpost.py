19675c6dd4225815442f7127d244df9a54ee9453
vashmeow <isanjuanivan@gmail.com>
2025-08-18 06:05:43 +0800

    created a class instead to prevent alot of repetition

--- apps/scraper_coolingpost.py
+++ apps/scraper_coolingpost.py
@@ -10,80 +10,87 @@ import pandas as pd
 import re
 import time
 
-today = date.today()
-yesterday = today-timedelta(days=-1)
-def get_cooling_post_news(driver, coverage_date):
-    driver.get('https://www.coolingpost.com/')
+class CoolingPostNews:
+    def __init__(self, news_link, coverage_date, driver, name, source):
+        self.news_link = news_link
+        self.coverage_date = coverage_date
+        self.driver = driver
+        self.soup = None
+        self.news_block = []
+        self.latest_news = []
+        self.page_num = 1
+        self.conditions_met = False
+        self.today = date.today()
+        self.name = name
+        self.source = source
     
-    latest_news = []
-    WebDriverWait(driver, timeout=5).until(
-        EC.presence_of_element_located((By.ID, 'main-content'))
-    )
-    href_world_news = driver.find_element(By.XPATH, '//*[@id="main-content"]/div/div[3]/div/div/h2/a').get_attribute('href')
-    driver.switch_to.new_window(WindowTypes.TAB)
-    driver.get(href_world_news)
-    worldnews_html = driver.page_source
-    worldnews_soup = BeautifulSoup(worldnews_html,'html.parser')
-    WebDriverWait(driver, timeout=5).until(
-        EC.presence_of_element_located((By.ID,'main-content'))
-    )
-    worldnews_block = worldnews_soup.find('div', id='main-content')
-    worldnews_section = worldnews_block.find_all('div', class_='cl-element-section')
-    for news in worldnews_section:
-        parsed_date = news.find('div', class_='cl-element-published_date').text
-        parsed_date_obj = datetime.strptime(re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date), '%d %B %Y')
-        worldnews_publish_date = parsed_date_obj.strftime('%Y-%m-%d')
-        if parsed_date_obj.date() >= today-timedelta(days=coverage_date):
-            worldnews_title=news.find('a', class_='cl-element-title__anchor')
-            worldnews_link=worldnews_title.get('href')
-            worldnews_summary=news.find('div', class_='cl-element-excerpt')
-            latest_news.append(
+    def get_soup(self):
+        while True:
+            url = self.news_link if self.page_num == 1 else f'https://www.coolingpost.com/{self.name}/page/{self.page_num}/'
+            self.driver.get(url)
+            WebDriverWait(self.driver,timeout=5).until(
+                EC.presence_of_element_located((By.ID,'main-content'))
+            )
+            html = self.driver.page_source
+            self.soup = BeautifulSoup(html,'html.parser')
+            if not self.get_news_blocks():
+                break
+            self.page_num+=1
+        
+    def get_news_blocks(self):
+        news_section = self.soup.find('div', id='main-content')
+        news_blocks = news_section.find_all('div', class_='cl-element-section')
+        for news in news_blocks:
+            parsed_date = news.find('div', class_='cl-element-published_date').text
+            parsed_date_obj = datetime.strptime(re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date), '%d %B %Y')
+            publish_date = parsed_date_obj.strftime('%Y-%m-%d')
+            if parsed_date_obj.date() < self.today-timedelta(days=self.coverage_date):
+                return False
+            self.title = news.find('a', class_='cl-element-title__anchor')
+            self.link = self.title.get('href')
+            self.summary = news.find('div', class_='cl-element-excerpt')
+            self.latest_news.append(
                 {
-                    'PublishDate': worldnews_publish_date,
-                    'Title': worldnews_title.text.strip(),
-                    'Source': 'CoolingPost-World',
-                    'Link': worldnews_link,
-                    'Summary': worldnews_summary.text.strip()
+                'PublishDate': publish_date,
+                'Title': self.title.text.strip(),
+                'Source': self.source,
+                'Link': self.link,
+                'Summary': self.summary.text.strip()
                 }
             )
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-            
-    href_uknews = driver.find_element(By.XPATH, '//*[@id="main-content"]/div/div[5]/div/div/h2/a').get_attribute('href')
-    driver.switch_to.new_window(WindowTypes.TAB)
-    driver.get(href_uknews)
-    uknews_html = driver.page_source
-    uknews_soup = BeautifulSoup(uknews_html,'html.parser')
-    WebDriverWait(driver, timeout=5).until(
-        EC.presence_of_element_located((By.ID, 'main-content'))
-    )
-    uknews_block = uknews_soup.find('div', class_='cl-layout-container')
-    uknews_section = uknews_block.find_all('div', class_='cl-element-section')
-    for news in uknews_section:
-        parsed_date = news.find('div', class_='cl-element-published_date').text
-        parsed_date_obj = datetime.strptime(re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date), '%d %B %Y')
-        uknews_publish_date = parsed_date_obj.strftime('%Y-%m-%d')
-        if parsed_date_obj.date() >= today-timedelta(days=coverage_date):
-            uknews_title = news.find('a', class_='cl-element-title__anchor')
-            uknews_link = uknews_title.get('href')
-            uknews_summary = news.find('div', class_='cl-element-excerpt')
-            latest_news.append(
-                {
-                    'PublishDate': uknews_publish_date,
-                    'Title': uknews_title.text.strip(),
-                    'Source': 'CoolingPost-UK',
-                    'Link': uknews_link,
-                    'Summary': uknews_summary.text.strip()
-                }
-            )
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+        return True
+                
+    def scrape(self):
+        self.get_soup()
 
-    df = pd.DataFrame(latest_news)
+all_news = []        
+def get_cooling_post_news(driver, coverage_date):
+    world = 'https://www.coolingpost.com/world-news/'
+    uk = 'https://www.coolingpost.com/uk-news/'
+    products = 'https://www.coolingpost.com/products/'
+    features = 'https://www.coolingpost.com/features/'
+    blog = 'https://www.coolingpost.com/blog/'
+    train_events = 'https://www.coolingpost.com/training/'
+    world_news = CoolingPostNews(world,coverage_date, driver,'world-news','CoolingPost-World')
+    uk_news = CoolingPostNews(uk,coverage_date,driver,'uk-news','CoolingPost-UK')
+    product_news = CoolingPostNews(products,coverage_date,driver,'products','CoolingPost-Products')
+    feature_news = CoolingPostNews(features,coverage_date,driver,'features','CoolingPost-Features')
+    blog_news = CoolingPostNews(blog,coverage_date,driver,'blog','CoolingPost-Blog')
+    train_events_news = CoolingPostNews(train_events,coverage_date,driver,'training','CoolingPost-T&E')
+    world_news.scrape()
+    uk_news.scrape()
+    product_news.scrape()
+    feature_news.scrape()
+    blog_news.scrape()
+    train_events_news.scrape()
+    all_news.extend(world_news.latest_news)
+    all_news.extend(uk_news.latest_news)
+    all_news.extend(product_news.latest_news)
+    all_news.extend(blog_news.latest_news)
+    all_news.extend(train_events_news.latest_news)
+    df = pd.DataFrame(all_news)
     df.to_csv('csv/cooling_post_news.csv', index=False)
     
-    return latest_news
-            
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--window-size=1920x1080')
@@ -92,8 +99,7 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115 Safari/537.36")
 
 driver = webdriver.Chrome(options=options)
-get_cooling_post_news(driver, coverage_date=15)
+get_cooling_post_news(driver, coverage_date=30)
 
 time.sleep(10)
 driver.quit()
-    
