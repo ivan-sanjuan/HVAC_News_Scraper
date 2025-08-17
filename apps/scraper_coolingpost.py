25a781862b90f1701dcad83faae1ff95be2ce965
vashmeow <isanjuanivan@gmail.com>
2025-08-17 21:40:09 +0800

    refactoring functions to accept date-range

--- apps/scraper_coolingpost.py
+++ apps/scraper_coolingpost.py
@@ -3,50 +3,97 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.window import WindowTypes
 from bs4 import BeautifulSoup
-import re
-from datetime import datetime, date, timedelta
+from datetime import date, timedelta, datetime
 import pandas as pd
+import re
 import time
 
 today = date.today()
 yesterday = today-timedelta(days=-1)
-
-def get_cooling_post_news(driver):
+def get_cooling_post_news(driver, coverage_date):
     driver.get('https://www.coolingpost.com/')
-
-    WebDriverWait(driver, timeout=5).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'cl-element-title__anchor'))
-    )
-
-    html_data = driver.page_source
-    soup = BeautifulSoup(html_data, 'html.parser')
-    news_section = soup.find('div', id='main-content')
-    news_block = news_section.find_all('div', class_='cl-template--image-top')
-
+    
     latest_news = []
-    for news in news_block:
-        title = news.find('a', class_='cl-element-title__anchor')
-        summary = news.find('div', class_='cl-element-excerpt')
-        link = title.get('href')
-        parsed_date = news.find('div', 'cl-element-published_date').text
-        parsed_date_cleaned = re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date)
-        old_format = '%d %B %Y'
-        new_format = '%Y-%m-%d'
-        parsed_date_formatted = datetime.strptime(parsed_date_cleaned, old_format)
-        publish_date = datetime.strftime(parsed_date_formatted, new_format)
-        
-        latest_news.append(
-            {
-                'PublishDate': publish_date,
-                'Source': 'Cooling Post',
-                'Title': title.text.strip(),
-                'Summary': summary.text.strip(),
-                'Link': link
-            }
-        )
-        
+    WebDriverWait(driver, timeout=5).until(
+        EC.presence_of_element_located((By.ID, 'main-content'))
+    )
+    href_world_news = driver.find_element(By.XPATH, '//*[@id="main-content"]/div/div[3]/div/div/h2/a').get_attribute('href')
+    driver.switch_to.new_window(WindowTypes.TAB)
+    driver.get(href_world_news)
+    worldnews_html = driver.page_source
+    worldnews_soup = BeautifulSoup(worldnews_html,'html.parser')
+    WebDriverWait(driver, timeout=5).until(
+        EC.presence_of_element_located((By.ID,'main-content'))
+    )
+    worldnews_block = worldnews_soup.find('div', id='main-content')
+    worldnews_section = worldnews_block.find_all('div', class_='cl-element-section')
+    for news in worldnews_section:
+        parsed_date = news.find('div', class_='cl-element-published_date').text
+        parsed_date_obj = datetime.strptime(re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date), '%d %B %Y')
+        worldnews_publish_date = parsed_date_obj.strftime('%Y-%m-%d')
+        if parsed_date_obj.date() >= today-timedelta(days=coverage_date):
+            worldnews_title=news.find('a', class_='cl-element-title__anchor')
+            worldnews_link=worldnews_title.get('href')
+            worldnews_summary=news.find('div', class_='cl-element-excerpt')
+            latest_news.append(
+                {
+                    'PublishDate': worldnews_publish_date,
+                    'Title': worldnews_title.text.strip(),
+                    'Source': 'CoolingPost-World',
+                    'Link': worldnews_link,
+                    'Summary': worldnews_summary.text.strip()
+                }
+            )
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+            
+    href_uknews = driver.find_element(By.XPATH, '//*[@id="main-content"]/div/div[5]/div/div/h2/a').get_attribute('href')
+    driver.switch_to.new_window(WindowTypes.TAB)
+    driver.get(href_uknews)
+    uknews_html = driver.page_source
+    uknews_soup = BeautifulSoup(uknews_html,'html.parser')
+    WebDriverWait(driver, timeout=5).until(
+        EC.presence_of_element_located((By.ID, 'main-content'))
+    )
+    uknews_block = uknews_soup.find('div', class_='cl-layout-container')
+    uknews_section = uknews_block.find_all('div', class_='cl-element-section')
+    for news in uknews_section:
+        parsed_date = news.find('div', class_='cl-element-published_date').text
+        parsed_date_obj = datetime.strptime(re.sub(r'(\d+)(st|nd|rd|th)', r'\1', parsed_date), '%d %B %Y')
+        uknews_publish_date = parsed_date_obj.strftime('%Y-%m-%d')
+        if parsed_date_obj.date() >= today-timedelta(days=coverage_date):
+            uknews_title = news.find('a', class_='cl-element-title__anchor')
+            uknews_link = uknews_title.get('href')
+            uknews_summary = news.find('div', class_='cl-element-excerpt')
+            latest_news.append(
+                {
+                    'PublishDate': uknews_publish_date,
+                    'Title': uknews_title.text.strip(),
+                    'Source': 'CoolingPost-UK',
+                    'Link': uknews_link,
+                    'Summary': uknews_summary.text.strip()
+                }
+            )
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
     df = pd.DataFrame(latest_news)
     df.to_csv('csv/cooling_post_news.csv', index=False)
-
+    
     return latest_news
+            
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920x1080')
+options.add_argument('--log-level=3')
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115 Safari/537.36")
+
+driver = webdriver.Chrome(options=options)
+get_cooling_post_news(driver, coverage_date=15)
+
+time.sleep(10)
+driver.quit()
+    
--- apps/scraper_refindustry.py
+++ apps/scraper_refindustry.py
@@ -3,62 +3,82 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.window import WindowTypes
 from bs4 import BeautifulSoup
 from datetime import date, timedelta, datetime
 import pandas as pd
 import time
 
-today = date.today()
-yesterday = today-timedelta(days=1)
-
-def get_refindustry_news(driver,coverage_date):
-    driver.get('https://refindustry.com/news/')
-
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'posts_new'))
-        )
-
-    html_data = driver.page_source
-    soup = BeautifulSoup(html_data, 'html.parser')
-    news_section = soup.find('div', class_='posts_new')
-    news_blocks = news_section.find_all('a', class_='post_new _post_news_status')
-
+def get_refindustry_news(driver, coverage_date):
     page_num = 1
+    conditions_met = False
     latest_news = []
-    for news in news_blocks:
-        parsed_date = news.find('div', class_='post_bot_text').text
-        original_format_date_obj = datetime.strptime(parsed_date, '%d %b %Y')
-        publish_date = datetime.strftime(original_format_date_obj,'%Y-%m-%d')
-        if parsed_date == 'today':
-            publish_date = today
-        elif parsed_date == 'yesterday':
-            publish_date = yesterday
+    today = date.today()
+    yesterday = today-timedelta(days=1)
+    while True:
+        if page_num == 1:
+            driver.get('https://refindustry.com/news/')
         else:
-            if publish_date >= coverage_date:
-                title = news.find('div', class_='post_title').text
-                summary = news.find('div', class_='post_descr')
-                link = news.get('href')
-                pages = driver.find_element(By.ID, 'pagination')
-                WebDriverWait(driver, timeout=5).until(
-                    EC.element_to_be_clickable((By.CLASS_NAME, 'active'))
+            driver.execute_script(f'goToPage({page_num})')
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'posts_new'))
                 )
-                page_num += 1
-                
-                latest_news.append(
-                    {
-                        'PublishDate': publish_date,
-                        'Source': 'RefIndustry',
-                        'Title': title.strip(),
-                        'Summary': summary.text.strip(),
-                        'Link': link
-                    }
-                )
-    
-    next_page = pages.find_element(By.CSS_SELECTOR, f'{page_num}')
-    next_page.click()
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            news_section = soup.find('div', class_='posts_new')
+            news_blocks = news_section.find_all('a', class_='post_new')
+            for news in news_blocks:
+                parsed_date = news.find('div', class_='post_bot_text').text
+                if parsed_date == 'today':
+                    publish_date = today
+                elif parsed_date == 'yesterday':
+                    publish_date = yesterday
+                else:
+                    old_format = '%d %b %Y'
+                    parsed_date_obj = datetime.strptime(parsed_date, old_format)
+                    publish_date = parsed_date_obj.strftime('%Y-%m-%d')
+                    max_date_range = today-timedelta(days=coverage_date)
+                    if parsed_date_obj.date() >= max_date_range:
+                        link = news.get('href')
+                        driver.switch_to.new_window(WindowTypes.TAB)
+                        driver.get(link)
+                        news_soup = BeautifulSoup(driver.page_source, 'html.parser')
+                        title = news_soup.find('h1', class_='page-title_new')
+                        WebDriverWait(driver, timeout=5).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, 'news-detail-full-text-block_new'))
+                        )
+                        summary_div = news_soup.find('div', class_='news-detail-full-text-block_new')
+                        if summary_div:
+                            summary_p = summary_div.find('p')
+                            if summary_p:
+                                summary = summary_p.get_text(strip=True)
+                            else:
+                                summary = summary_div.get_text(strip=True)
+                        else:
+                            summary = None
+                        
+                        latest_news.append(
+                            {
+                            'PublishDate': publish_date,
+                            'Title': title.text.strip(),
+                            'Source': 'Ref Industry',
+                            'Link': link,
+                            'Summary': summary
+                            }
+                        )
+                        
+                        driver.close()
+                        driver.switch_to.window(driver.window_handles[0])
+                    else:
+                        conditions_met = True
+                        break
 
+            if conditions_met == True:
+                break
+        
+        page_num += 1
+            
+    print(latest_news)
+    
     df = pd.DataFrame(latest_news)
     df.to_csv('csv/ref_industry_news.csv', index=False)
-    
     return latest_news
-
