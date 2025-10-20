cb8eab0432beb366c8d7e33dc1a40d5ebe87fc2a
vashmeow <isanjuanivan@gmail.com>
2025-10-21 03:11:01 +0800

    continued to refactor function

--- apps/scraper_bitzer.py
+++ apps/scraper_bitzer.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from datetime import timedelta, datetime
 import pandas as pd
 import time
@@ -17,12 +18,13 @@ class BitzerNews:
         self.url = url
         self.latest_news = []
         self.date_limit = datetime.today()-timedelta(days=self.coverage)
+        self.root = 'https://www.bitzer.de/'
     
     def get_soup(self):
         print(f'📰Opening: Bitzer')
         self.driver.get(self.url)
         try:
-            accept_cookie = WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.ID,'uc-btn-accept-banner')))
+            accept_cookie = self.driver_wait(EC.element_to_be_clickable((By.ID,'uc-btn-accept-banner')))
             self.driver.execute_script("arguments[0].scrollIntoView();",accept_cookie)
             accept_cookie.click()
         except Exception as e:
@@ -31,67 +33,47 @@ class BitzerNews:
         html = self.driver.page_source
         soup = BeautifulSoup(html,'html.parser')
         news_section = soup.find('div', id='paging-content')
-        self.news_blocks = news_section.find_all('section',class_='news')
-        self.news_blocks_sel = self.driver.find_elements(By.CLASS_NAME, 'news')
+        news_blocks = news_section.find_all('section',class_='news')
+        news_blocks_sel = self.driver.find_elements(By.CLASS_NAME, 'news')
+        self.get_news(news_blocks,news_blocks_sel)
     
-    def open_to_new_tab(self,link):
-        ActionChains(self.driver)\
-        .key_down(Keys.CONTROL)\
-        .click(link)\
-        .key_up(Keys.CONTROL)\
-        .perform()
-    
-    def get_summary(self):
-        html = self.driver.page_source
-        soup = BeautifulSoup(html,'html.parser')
-        title = soup.find('h1',class_='overview-headline').text.strip()
-        summary_block = soup.find('div',class_='press-information-content')
-        paragraphs = summary_block.find_all('p')
-        print(f'Fetching News: {title}')
-        for sum in paragraphs:
-            if len(sum.text.strip()) < 150:
-                pass
-            else:
-                summary = sum.text.strip()
-                
-        return ({'title':title,'summary':summary})
-    
-    def get_news(self):
-        for news, section in zip(self.news_blocks,self.news_blocks_sel):
+    def get_news(self,bs4_block,sel_block):
+        for news, section in zip(bs4_block,sel_block):
             parsed_date = news.find('span',class_='press-news-span').text.strip()
             parsed_date_obj = datetime.strptime(parsed_date,'%d.%m.%Y')
             publish_date = parsed_date_obj.strftime('%Y-%m-%d')
             if parsed_date_obj >= self.date_limit:
                 self.driver.execute_script("arguments[0].scrollIntoView();",section)
                 link_sel = section.find_element(By.LINK_TEXT,'Read more')
-                link = news.find('a',class_='primary-color').get('href')
-                self.open_to_new_tab(link_sel)
-                self.driver.switch_to.window(self.driver.window_handles[1])
-                current_tabs = self.driver.window_handles
-                try:
-                    WebDriverWait(self.driver,3).until(lambda e: len(e.window_handles) > len(current_tabs))
-                except:
-                    pass
-                page = self.get_summary()
-                title = page.get('title')
-                summary = page.get('summary')
-                self.driver.close()
-                self.driver.switch_to.window(self.driver.window_handles[0])
-                root_url='https://www.bitzer.de'
-                self.latest_news.append(
-                    {
-                    'PublishDate':publish_date,
-                    'Source': 'Bitzer',
-                    'Type': 'Company News',
-                    'Title': title,
-                    'Summary': summary,
-                    'Link': f'{root_url}{link}'
-                    }
-                )
+                link = news.find('a',class_='primary-color')
+                if link:
+                    link = link.get('href')
+                    link = urljoin(self.root,link)
+                title = news.find('h2',class_='news-headline').text.strip()
+                summary = news.find('p').text.strip()
+                self.append(publish_date,title,summary,link)
+    
+    def append(self,publish_date,title,summary,link):
+        print(f'Fetching: {title}')
+        self.latest_news.append(
+            {
+            'PublishDate':publish_date,
+            'Source': 'Bitzer',
+            'Type': 'Company News',
+            'Title': title,
+            'Summary': summary,
+            'Link': link
+            }
+        )
+    
+    def driver_wait(self,condition):
+        try:
+            return WebDriverWait(self.driver,5).until(condition)
+        except:
+            pass
     
     def scrape(self):
         self.get_soup()
-        self.get_news()
 
 all_news = []
 def get_bitzer_news(driver,coverage_days):
@@ -101,5 +83,5 @@ def get_bitzer_news(driver,coverage_days):
     news.scrape()
     all_news.extend(news.latest_news)
     df = pd.DataFrame(all_news)
+    df = df.drop_duplicates(subset=['Link'])
     df.to_csv('csv/bitzer_news.csv',index=False)
-
