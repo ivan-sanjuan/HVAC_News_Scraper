a2ba08224e96a3f312fd820874a2e20599d2b8cd
vashmeow <isanjuanivan@gmail.com>
2025-10-22 04:23:33 +0800

    minor change

--- apps/scraper_LG_HVAC.py
+++ apps/scraper_LG_HVAC.py
@@ -58,7 +58,6 @@ class LGHVACNews:
                 self.append(publish_date,title,summary,link)
             else:
                 self.strike += 1
-                print(self.strike)
                 if self.strike == 3:
                     return False
     
@@ -98,17 +97,3 @@ def get_LGHVAC_NA(driver,coverage_days):
     df = pd.DataFrame(all_news)
     df = df.drop_duplicates(subset=['Link'])
     df.to_csv('csv/LG_HVAC_NA_news.csv',index=False)
-    
-options = Options()
-options.add_argument('--headless=new')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1920x1080')
-options.add_argument('--log-level=3')
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4")
-options.page_load_strategy = 'eager'
-driver = webdriver.Chrome(options=options)
-get_LGHVAC_NA(driver,coverage_days=600)
-
-time.sleep(5)
-driver.quit()
