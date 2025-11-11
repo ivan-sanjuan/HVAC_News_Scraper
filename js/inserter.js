1e7ef10b2698fff63d1867e0f77c5b271a42e49b
Ivan San Juan <isanjuanivan@gmail.com>
2025-11-12 01:50:06 +0800

    javascript exercises

--- js/inserter.js
+++ js/inserter.js
@@ -1,5 +1,38 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('code').textContent = 'this is from a separate inserter.js';
-})
+const name = 'Ivan'
+const age = 34
+
+function declareName(){
+    console.log(`My name is ${name}, i am ${age}`);
+}
+
+declareName();
+
+var x=30;
+var y=40;
+function addOperation(numOne,numTwo) {
+    console.log('Sum is: ', numOne+numTwo);
+}
+
+addOperation(x,y);
+addOperation(100,5);
+
+var array1 = [40,77,141,642,2132,6139]
+
+function evenOdd(array){
+    for (let num of array){
+        if (num % 2 == 0){
+            console.log('This is even number: ',num);
+        }
+        else{
+            console.log('This is odd: ',num);
+        }
+    }
+}
+
+evenOdd(array1);
+
+var array1 = [213,53214,5132,126,73553]
+
+evenOdd(array1);
