c954cb80ebd62efc308d3e1642681d7c21ec20d4
Ivan San Juan <isanjuanivan@gmail.com>
2025-11-14 01:04:21 +0800

    new set of exercises JS

--- js/inserter.js
+++ js/inserter.js
@@ -93,49 +93,66 @@
 //     }
 // }
 
-var phrase = 'supercalifragilisticexpialidocious';
-var vowels = ['a','e','i','o','u'];
-var counter = 0;
+// var phrase = 'supercalifragilisticexpialidocious';
+// var vowels = ['a','e','i','o','u'];
+// var counter = 0;
 
-for(let char of phrase){
-    if(vowels.includes(char)){
-        counter += 1;
-    }
-}
-// console.log(counter);
-
-var paragraph = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour";
-var wordsArray = paragraph.split(' ');
-let longestWord = "";
-for(let word of wordsArray){
-    if(word.length > longestWord.length){
-        longestWord = word;
-    }
-}
-// console.log(longestWord);
-
-var words = ['every','good','boy','does','fine']
-// for(let word of words){
-//     let upperWord = word.charAt(0).toUpperCase() + word.slice(1);
-//     // console.log(upperWord)
-// }
-
-var numArray = [213,4311,2315,123,4635,-12316];
-// var absnumArray = numArray.map(num => Math.abs(num));
-// var largest_num = 0;
-// for(let num of absnumArray){
-//     if(num > largest_num){
-//         largest_num = num;
+// for(let char of phrase){
+//     if(vowels.includes(char)){
+//         counter += 1;
 //     }
 // }
-// var absValues = numArray.map((num) => Math.abs(num))
-// console.log(largest_num)
+// // console.log(counter);
 
-for(let word of words){
-    let capitalized = word.charAt(0).toUpperCase() + word.slice(1);
-    console.log(capitalized)
-}
+// var paragraph = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour";
+// var wordsArray = paragraph.split(' ');
+// let longestWord = "";
+// for(let word of wordsArray){
+//     if(word.length > longestWord.length){
+//         longestWord = word;
+//     }
+// }
+// // console.log(longestWord);
 
-var ceilingNum = numArray.map((num) => Math.sqrt(Math.abs(num)))
-var forEachNum = numArray.forEach((num) => num+1000);
-console.log(forEachNum);
+// var words = ['every','good','boy','does','fine']
+// // for(let word of words){
+// //     let upperWord = word.charAt(0).toUpperCase() + word.slice(1);
+// //     // console.log(upperWord)
+// // }
+
+// var numArray = [213,4311,2315,123,4635,-12316];
+// // var absnumArray = numArray.map(num => Math.abs(num));
+// // var largest_num = 0;
+// // for(let num of absnumArray){
+// //     if(num > largest_num){
+// //         largest_num = num;
+// //     }
+// // }
+// // var absValues = numArray.map((num) => Math.abs(num))
+// // console.log(largest_num)
+
+// for(let word of words){
+//     let capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+//     console.log(capitalized)
+// }
+
+// var ceilingNum = numArray.map((num) => Math.sqrt(Math.abs(num)))
+// var forEachNum = numArray.forEach((num) => num+1000);
+// console.log(forEachNum);
+
+
+var nums = [6.5,71.34,12.01,9.86,34.97]
+var addedNums = nums.map((num) => Math.round(num)+5)
+console.log(addedNums)
+
+var combinedNums = addedNums.reduce((sum,a) => sum+a)
+console.log(combinedNums)
+
+var strSample = 'hello';
+var rearranged = strSample.split('').toReversed().join('');
+var capStr = strSample.charAt(0).toUpperCase() + strSample.slice(1);
+console.log(capStr)
+
+var strArray = ['hello','i','am','hitchcock'];
+var strArrayCap = strArray.map((str) => str.charAt(0).toUpperCase() + str.slice(1));
+console.log(strArrayCap);
